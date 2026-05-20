ef4c3671990bf62d8b67658aa988eddc893c1666
septech <septechx@proton.me>
2026-05-21 00:19:34 +0200

    feat: Support creating tmux sessions

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,13 @@ pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
     const io = init.io;
 
-    const file = std.Io.Dir.cwd().readFileAlloc(io, "oxi.glass", gpa, .unlimited) catch unreachable;
+    // TODO: Get this as argument
+    const name = "testing";
+
+    const file_name = try std.fmt.allocPrint(gpa, "{s}.glass", .{name});
+    defer gpa.free(file_name);
+
+    const file = std.Io.Dir.cwd().readFileAlloc(io, file_name, gpa, .unlimited) catch unreachable;
     defer gpa.free(file);
 
     const file_cstr = gpa.alloc(u8, file.len + 1) catch unreachable;
@@ -24,14 +30,25 @@ pub fn main(init: std.process.Init) !void {
     const res = c.glass_parse(file_cstr.ptr);
     defer c.glass_result_free(res);
 
-    if (c.glass_result_get_kind(res) == c.GLASS_RESULT_ERROR) {
+    if (c.glass_result_get_kind(res) == c.GLASS_RESULT_ERROR)
         return error.InvalidGlassFile;
-    }
 
     const value = c.glass_result_value(res);
 
     const cfg = try config.Config.parse(@ptrCast(&value[0]), gpa);
     defer cfg.free(gpa);
 
-    std.debug.print("{s}\n", .{cfg.windows[1].cmd});
+    try process_cfg(io, gpa, name, cfg);
+}
+
+fn process_cfg(io: Io, gpa: Allocator, name: []const u8, cfg: config.Config) !void {
+    try tmux.create_session(io, name);
+
+    for (0..cfg.windows.len) |i| {
+        const cmd = cfg.windows[i].cmd;
+        const index: u32 = @truncate(i + 1);
+        if (index > 1) try tmux.create_window(io, gpa, name, index);
+        if (cmd.len != 0)
+            try tmux.execute_command(io, gpa, name, index, cfg.windows[i].cmd);
+    }
 }
--- src/tmux.zig
+++ src/tmux.zig
@@ -1,14 +1,37 @@
 const std = @import("std");
 const Io = std.Io;
+const Allocator = std.mem.Allocator;
 
 pub fn create_session(io: Io, name: []const u8) !void {
-    _ = try std.process.spawn(io, .{
-        .argv = &[_][]const u8{
-            "tmux",
-            "new",
-            "-s",
-            name,
-            "-d",
-        },
-    });
+    _ = try std.process.spawn(io, .{ .argv = &.{
+        "tmux",
+        "new",
+        "-s",
+        name,
+        "-d",
+    } });
+}
+
+pub fn create_window(io: Io, gpa: Allocator, session: []const u8, index: u32) !void {
+    const window = try std.fmt.allocPrint(gpa, "{s}:{}", .{ session, index });
+    defer gpa.free(window);
+    _ = try std.process.spawn(io, .{ .argv = &.{
+        "tmux",
+        "new-window",
+        "-t",
+        window,
+    } });
+}
+
+pub fn execute_command(io: Io, gpa: Allocator, session: []const u8, index: u32, cmd: []const u8) !void {
+    const window = try std.fmt.allocPrint(gpa, "{s}:{}", .{ session, index });
+    defer gpa.free(window);
+    _ = try std.process.spawn(io, .{ .argv = &.{
+        "tmux",
+        "send-keys",
+        "-t",
+        window,
+        cmd,
+        "C-m",
+    } });
 }
